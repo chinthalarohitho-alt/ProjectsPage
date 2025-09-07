57a96e1c012330691e40da76c2314def954ebfe5
rohithkumaro <rohith.kumar@yellow.ai>
2025-09-07 14:22:49 +0530

    first commit with 2 projects

---
+++ javaScript.js
@@ -0,0 +1,32 @@
+let ticTacToe = document.querySelector(".ticTacToe");
+let ticTacToeRepo = document.querySelector(".ticTacToeRepo");
+let CurrencyConverter = document.querySelector(".CurrencyConverter");
+let CurrencyConverterRepo = document.querySelector(".CurrencyConverterRepo");
+
+ticTacToe.addEventListener("click", () => {
+  window.open(
+    "https://chinthalarohitho-alt.github.io/ticTacToeGame/",
+    "_blank"
+  );
+});
+
+ticTacToeRepo.addEventListener("click", () => {
+  window.open(
+    "https://github.com/chinthalarohitho-alt/ticTacToeGame",
+    "_blank"
+  );
+});
+
+CurrencyConverter.addEventListener("click", () => {
+  window.open(
+    "https://chinthalarohitho-alt.github.io/CurrencyConverter/",
+    "_blank"
+  );
+});
+
+CurrencyConverterRepo.addEventListener("click", () => {
+  window.open(
+    "https://github.com/chinthalarohitho-alt/CurrencyConverter",
+    "_blank"
+  );
+});
